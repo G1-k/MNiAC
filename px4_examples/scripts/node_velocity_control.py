99487878a3570c09e0ef656150e251529ab8f33d
Jeevan K <iamjeevank2000@gmail.com>
2021-02-09 21:29:56 +0530

    updated velocity control

--- px4_examples/scripts/node_velocity_control.py
+++ px4_examples/scripts/node_velocity_control.py
@@ -70,6 +70,14 @@ class fcuModes:
         except rospy.ServiceException, e:
             print "service set_mode call failed: %s. Position Mode could not be set."%e
 
+    def setAutoTakeoffMode(self):
+        rospy.wait_for_service('mavros/set_mode')
+        try:
+            flightModeService = rospy.ServiceProxy('mavros/set_mode', mavros_msgs.srv.SetMode)
+            flightModeService(custom_mode='AUTO.TAKEOFF')
+        except rospy.ServiceException, e:
+               print "service set_mode call failed: %s. Autotakeoff Mode could not be set."%e
+
     def setAutoLandMode(self):
         rospy.wait_for_service('mavros/set_mode')
         try:
@@ -78,16 +86,29 @@ class fcuModes:
         except rospy.ServiceException, e:
                print "service set_mode call failed: %s. Autoland Mode could not be set."%e
 
+    def change_mav_frame(self):
+        rospy.wait_for_service('/mavros/setpoint_velocity/mav_frame')
+        try:
+            frame = rospy.ServiceProxy('/mavros/setpoint_velocity/mav_frame', mavros_msgs.srv.SetMavFrame)
+            resp = frame(8)
+            return resp
+        except rospy.ServiceException as e:
+            print("Service call failed")
+
+
 class Controller:
     # initialization method
     def __init__(self):
         # Drone state
         self.state = State()
         self.vel = TwistStamped()
-        speed = 4
-        radius = 2
-        self.vel.twist.linear.x = speed
-        self.vel.twist.angular.z = speed/radius
+
+        self.vel.twist.linear.y = 0
+        self.vel.twist.linear.z = 0
+        self.vel.twist.linear.x = 0
+        self.vel.twist.angular.z = 0
+        self.vel.twist.angular.x = 0
+        self.vel.twist.angular.y = 0
         # Instantiate a setpoints message
         self.sp = PositionTarget()
         # # set the flag to use position setpoints and yaw angle
@@ -176,32 +197,48 @@ def main():
     vel_pub = rospy.Publisher(
             '/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=1)
 
+    modes.change_mav_frame()
 
     # Make sure the drone is armed
-    while not cnt.state.armed:
-        modes.setArm()
-        rate.sleep()
+    # while not cnt.state.armed:
+    #     modes.setArm()
+    #     rate.sleep()
 
     # set in takeoff mode and takeoff to default altitude (3 m)
-    modes.setOffboardMode()
-    modes.setTakeoff()
+    # modes.setOffboardMode()
+    #modes.setTakeoff()
     # rate.sleep()
 
     # We need to send few setpoint messages, then activate OFFBOARD mode, to take effect
+
+
+    while not cnt.state.armed:
+        modes.setArm()
+        rate.sleep()
+    # activate OFFBOARD mode
+    modes.setAutoTakeoffMode()
+    #modes.setOffboardMode()
+
     k=0
     while k<10:
         #sp_pub.publish(cnt.sp)
         vel_pub.publish(cnt.vel)
         rate.sleep()
         k = k + 1
+    
 
-    # activate OFFBOARD mode
-    #modes.setOffboardMode()
 
     # ROS main loop
     while not rospy.is_shutdown():
         #cnt.updateSp()
         #sp_pub.publish(cnt.sp)
+        if cnt.local_pos.z > 2:
+            rospy.loginfo("OFFBOARD")
+            modes.setOffboardMode()
+        speed = 4
+        radius = 2
+        cnt.vel.twist.linear.x = speed
+        cnt.vel.twist.angular.z = - speed/radius
         vel_pub.publish(cnt.vel)
         rate.sleep()
 
